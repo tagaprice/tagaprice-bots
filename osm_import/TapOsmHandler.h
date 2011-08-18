d0b7035cc54f353a4b1f76d3a265e2fa85d0a8c5
Manuel Reithuber <git@manuel.reithuber.net>
2011-08-18 17:10:12 +0200

    implemented generic tag support to TapOsmHandler, printing the statistics to stderr now

--- osm_import/TapOsmHandler.h
+++ osm_import/TapOsmHandler.h
@@ -1,27 +1,24 @@
+#include "CategoryMapper.h"
+#include "CouchOutput.h"
+
+#include <QDir>
+
 #include <osmium.hpp>
 #include <osmium/handler/coordinates_for_ways.hpp>
 #include <osmium/storage/byid.hpp>
-
-#include "CategoryMapper.h"
-#include "CouchOutput.h"
 
 typedef Osmium::Storage::Mmap<Osmium::OSM::Position> TapOsmHandler_NodePosStorage;
 
 class TapOsmHandler: public Osmium::Handler::Base {
 private:
-	int m_amenityCount;
-	int m_shopCount;
-	int m_bothCount;
-
 	int m_nodeCount;
-	int m_relationCount;
 	int m_wayCount;
 
 	const char *m_nodeStr;
-	const char *m_relationStr;
 	const char *m_wayStr;
 
 	QMap<QString, CategoryMapper*> m_categoryMappers;
+	QMap<QString, int> m_tagStats;
 
 	TapOsmHandler_NodePosStorage m_posNodes;
 	TapOsmHandler_NodePosStorage m_negNodes;
@@ -33,16 +30,10 @@ public:
 	 * \brief Constructor; Initializes the values of the statistic counters
 	 */
 	TapOsmHandler(): m_coordinateHandler(m_posNodes, m_negNodes) {
-		m_amenityCount = 0;
-		m_shopCount = 0;
-		m_bothCount = 0;
-
 		m_nodeCount = 0;
 		m_wayCount = 0;
-		m_relationCount = 0;
 
 		m_nodeStr = "node";
-		m_relationStr = "relation";
 		m_wayStr = "way";
 
 		_initMappers();
@@ -77,47 +68,42 @@ public:
 	}
 
 	void _initMappers() {
-		m_categoryMappers.insert("amenity", new CategoryMapper("mappers/amenity.ini"));
-		m_categoryMappers.insert("shop", new CategoryMapper("mappers/shop.ini"));
+		QDir mappersDir("mappers/");
+		foreach (QString filename, mappersDir.entryList(QStringList("*.ini"), QDir::Files)) {
+			QFileInfo fileInfo(mappersDir.absoluteFilePath(filename));
+			QString tagName = fileInfo.baseName();
+			m_categoryMappers.insert(tagName, new CategoryMapper(fileInfo.absoluteFilePath()));
+			m_tagStats.insert(tagName, 0);
+		}
 	}
 
 	void _object(const Osmium::OSM::Object *object, const char *typeStr) {
-		const char *amenityValue = object->tags().get_tag_by_key("amenity");
-		const char *shopValue = object->tags().get_tag_by_key("shop");
 		const char *value = 0;
 		QString categoryId;
 
-		if (shopValue||amenityValue) {
-			//printf("%s %i (rev: %i, user: %s)\n", typeStr, object->id(), object->version(), object->user());
-			if (amenityValue) {
-				//printf("\tamenity=%s\n", amenityValue);
-				CategoryMapper *mapper = m_categoryMappers["amenity"];
-				if (mapper->hasCategory(amenityValue)) {
-					m_amenityCount++;
-					value = amenityValue;
-					categoryId = mapper->getCategoryId(amenityValue);
-				}
-			}
-			if (shopValue) {
-				//printf("\tshop=%s\n", shopValue);
-				m_shopCount++;
-				CategoryMapper *mapper = m_categoryMappers["shop"];
-				if (mapper->hasCategory(shopValue)) {
-					value = shopValue;
-					categoryId = mapper->getCategoryId(shopValue);
-				}
-			}
-			if (amenityValue && shopValue) {
-				m_bothCount++;
-			}
+		// try to find a matching tag
+		foreach (QString tagName, m_categoryMappers.keys()) {
+			CategoryMapper *mapper = m_categoryMappers[tagName];
+			const char *curValue = object->tags().get_tag_by_key(tagName.toUtf8().constData());
+			if (curValue) {
+				if (mapper->hasCategory(curValue)) {
+					QString curCategoryId = mapper->getCategoryId(curValue);
+					value = curValue;
+					m_tagStats[tagName]++;
 
+					if (!curCategoryId.isEmpty()) {
+						categoryId = curCategoryId;
+						break;
+					}
+				}
+			}
+		}
+
+		if (value) {
 			if (typeStr == m_nodeStr) m_nodeCount++;
-			else if (typeStr == m_relationStr) m_relationCount++;
 			else if (typeStr == m_wayStr) m_wayCount++;
 
-			if (value) {
-				CouchOutput::addObject(Entity(object, typeStr, categoryId));
-			}
+			CouchOutput::addObject(Entity(object, typeStr, categoryId));
 		}
 	}
 
@@ -125,14 +111,13 @@ public:
 	 * \brief print some statistics (after everything else has finished)
 	 */
 	void final() {
-		printf("Statistics:\n");
-		printf("\tAmenities: %i\n", m_amenityCount);
-		printf("\tShops    : %i\n", m_shopCount);
-		printf("\tBoth     : %i\n", m_bothCount);
-		printf("\n");
-		printf("Types:\n");
-		printf("\tNodes: %i\n", m_nodeCount);
-		printf("\tRelations: %i\n", m_relationCount);
-		printf("\tWays: %i\n", m_wayCount);
+		fprintf(stderr, "Statistics:\n");
+		foreach (QString tagName, m_tagStats.keys()) {
+			fprintf(stderr, "\t%s: %i\n", tagName.toLocal8Bit().constData(), m_tagStats[tagName]);
+		}
+		fprintf(stderr, "\n");
+		fprintf(stderr, "Types:\n");
+		fprintf(stderr, "\tNodes: %i\n", m_nodeCount);
+		fprintf(stderr, "\tWays: %i\n", m_wayCount);
 	}
 };
